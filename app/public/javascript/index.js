922b160a877c4b20615f6bd572680bb1f29428fa
Kevin Brannan <brannake@gmail.com>
2017-08-06 13:09:40 -0700

    Adds menu ajax

--- app/public/javascript/index.js
+++ app/public/javascript/index.js
@@ -1,13 +1,22 @@
+function startMenuCall () {
+    $.ajax({
+        url: "heroku.app.com",
+        method: "GET"
+    }).done(function(response) {
+        console.log(response);    
+        });
+    });
+}
+
 $(document).ready(function(){
 
     //Open the start button into the overlay
     $("#search-submit-shadow").click(function() {
         event.preventDefault();
-        $(this).animate({height: "50%", width: "70%", top: "10%", left: "15%", sopacity:"0.7"}, 500);
+        $(this).animate({height: "50%", width: "70%", top: "10%", left: "15%", sopacity:"0.7"});
         $("#home-prompt-1").animate({opacity:"0"});
         $("#home-prompt-2").animate({opacity:"0"});
         $("#search-submit").animate({opacity:"0"});
         $("#search-submit-shadow").animate({opacity:"0.7"});
-    });
-
+        startMenuCall();
 })
