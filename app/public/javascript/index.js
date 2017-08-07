a6dc774399f672ad00e55c126ef33ce43744cd15
Kevin Brannan <brannake@gmail.com>
2017-08-06 21:35:31 -0700

    When you click on the green buttons it adds filters

--- app/public/javascript/index.js
+++ app/public/javascript/index.js
@@ -19,7 +19,7 @@ function placeElementsOnHomeOverlay(username, calories, caloriesAllowed) {
     var search = $('<input/>').attr({ type: "text", name:"search", value:"Search", id: "menu-search"});
     $("#search-submit-shadow").append(search);
     var filter = $('<div/>').attr({id: "filter-search"});
-    filter.html("Available Filters:");
+    filter.html("Filter by:");
     $("#search-submit-shadow").append(filter);
     var filterContainer = $('<div/>').attr({id: "filter-container"});
     filterContainer.html("Filters Applied:")
@@ -51,12 +51,22 @@ function addFilterEventHandlers(arrFilters) {
             var filterText = $(this).text();
             var selectedFilter = $("<div>"+filterText+"</div>");
             selectedFilter.attr({id:"selected-" + filterText, class: "selected-Filters"});
-            var filterTextInput = $('<input/>').attr({ type: "text", name:"filter", value:"", id: "filter"});
-            var plusIcon = $('<img>').attr({src: "images/green_plus.png", id: "green-plus"});
+            var filterTextInput = $('<input/>').attr({ type: "text", name:"filter", value:"", id: "input-"+filterText});
+            var plusIcon = $('<img>').attr({src: "images/green_plus.png", class: "green-plus", id: "green-plus-"+filterText});
             $("#filter-container").append(selectedFilter);
             $(selectedFilter).append("<br/>");
             $(selectedFilter).append(filterTextInput);
             $(selectedFilter).append(plusIcon);
+            $("#filter-container").css("visibility", "visible");
+            
+            //Adds event handler to remove green button upon submission
+            $("#green-plus-"+filterText).click(function() {
+                console.log(this);
+                $(this).css("visibility", "hidden");
+                $("#input-"+filterText).css("visibility", "hidden");
+                var userFilterInput = $("#input-"+filterText).val().trim();
+                $("#selected-"+filterText).text(userFilterInput);
+            });   
         });
     };
 };
