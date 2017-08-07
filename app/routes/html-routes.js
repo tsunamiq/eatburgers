8166284764d8abb44899387e7f5effc3e92f4204
Jonathan Thinh <jonathan.thinh@gmail.com>
2017-08-06 21:15:02 -0700

    corrected html routes files to route to main page

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -12,9 +12,9 @@ var path = require("path");
 module.exports = function(app) {
 
  
-  // app.use("/", function(req, res) {
-  //       res.sendFile(path.join(__dirname, "../public/index.html"));
-  // });
+  app.use("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
  
 
