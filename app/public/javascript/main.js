0aefce9cd9de469691d7a56b28cc8de11a02f882
Kevin Brannan <brannake@gmail.com>
2017-08-11 19:47:28 -0700

    appends restaurant data to main

--- app/public/javascript/main.js
+++ app/public/javascript/main.js
@@ -7,206 +7,34 @@ function startMenuCall () {
         console.log(response);    
         });
     };
-
-//Sends to SignUp Page
-function  connectToSignUpPage() {
-    $.get("/api/form", function(data) {
-        console.log(data);
-        window.location.href = "/login.html";   
-        });
-    };
-
-
-function buildWireFrame(user_id) {
-    //==========================================================================
-    //       User Row
-    //==========================================================================
-
-    var rowOne = $("<div></div>").attr({class: "row userRow", id:user_id});
-
-    var colOneOne = $("<div></div>").attr({id: "user-name", class: "col s3"});
-    var colOneZero  = $("<div></div>").attr({class: "col s3"});
-    var colOneTwo = $("<div></div>").attr({id: "fixed-calories", class: "col s2"});
-    var colOneThree = $("<div>Consumed:</div>").attr({id: "calories-consumed", class: "col s2"});
-    var colOneFour = $("<div>Calories Left:</div>").attr({id: "calories-left", class: "col s2"});
-    $(rowOne).append(colOneOne);
-    $(rowOne).append(colOneZero);
-    $(rowOne).append(colOneTwo);
-    $(rowOne).append(colOneThree);
-    $(rowOne).append(colOneFour);
-    $("#search-submit-shadow").append(rowOne);
-
-    //==========================================================================
-    //       Restuarant  Row
-    //==========================================================================
-
-
-    var rowTwo = $("<div></div>").attr({id: "results-display-header", class: "row"});
-    var colTwoZero = $("<div></div>").attr({class: "col s1"});
-    var colTwoOne = $("<div>Restaurant Name</div>").attr({class: "col s3"});
-    var colTwoTwo = $("<div>Item</div>").attr({class: "col s2"});
-    var colTwoThree = $("<div>Calories</div>").attr({class: "col s2"});
-    var colTwoFour = $("<div>Price</div>").attr({class: "col s2"});
-    var colTwoFive = $("<div>Location</div>").attr({class: "col s2"});
-    $(rowTwo).append(colTwoZero);
-    $(rowTwo).append(colTwoOne);
-    $(rowTwo).append(colTwoTwo);
-    $(rowTwo).append(colTwoThree);
-    $(rowTwo).append(colTwoFour);
-    $(rowTwo).append(colTwoFive)
-    $("#search-submit-shadow").append(rowTwo);
-    var rowThree = $("<div></div>").attr({id: "results-display", class: "row"});
-    var colThreeZero = $("<div></div>").attr({class: "col s1"});
-    var colThreeOne = $("<div></div>").attr({class: "col s3"});
-    var colThreeTwo = $("<div></div>").attr({class: "col s2"});
-    var colThreeThree = $("<div></div>").attr({class: "col s2"});
-    var colThreeFour = $("<div></div>").attr({class: "col s2"});
-    var colThreeFive = $("<div></div>").attr({class: "col s2"});
-    $(rowThree).append(colThreeZero);
-    $(rowThree).append(colThreeOne);
-    $(rowThree).append(colThreeTwo);
-    $(rowThree).append(colThreeThree);
-    $(rowThree).append(colThreeFour);
-    $(rowThree).append(colThreeFive)
-    $("#search-submit-shadow").append(rowThree);
-
-    //==========================================================================
-    //          User Info Data
-    //==========================================================================
-    var userIcon = $("<img src='images/user-512.png'>").attr({id: "user-icon"});
-    $(colOneOne).append(userIcon);
-    var userName = $("<div>Allyson Felix</div>").attr({id: "user-name-1"});
-    $(colOneOne).append(userName);
-    var caloriesFixedHeader = $("<div>Daily Calories:</div>").attr({id: "calories-fixed-header"});
-    var caloriesFixedValue= $("<div>2500</div>").attr({id: "calories-fixed-value"});
-    $(colOneTwo).append(caloriesFixedHeader);
-    $(colOneTwo).append(caloriesFixedValue);
-    var caloriesConsumedHeader = $("<div>2000</div>");
-    $(colOneThree).append(caloriesConsumedHeader);
-    var caloriesLeft = $("<div>500</div>");
-    $(colOneFour).append(caloriesLeft);
-
-
-    //==========================================================================
-    //          Resturant Data
-    //==========================================================================
-
-    var restaurantName = $("<div>McDonald's</div>").attr({id: "restaurant-name"});
-    $(colThreeOne).append(restaurantName);
-    var foodItemName = $("<div>Big Mac</div>").attr({id: "calories-fixed"});
-    $(colThreeTwo).append(foodItemName);
-    var calorieValue = $("<div>1200</div>").attr({id: "calorie-value"});
-    $(colThreeThree).append(calorieValue);
-    var price = $("<div>$12.99</div>").attr({id: "calorie-value"});
-    $(colThreeFour).append(price);
-    var Location = $("<div>6256 Greenwich Ave</div>").attr({id: "location"});
-    $(colThreeFive).append(Location);
-}
-
-//Adds the filters options next to the search bar
-function addFilters() {
-    var arrFilters = ["Restaurants", "Calories", "Price"];
-    for (i=0; i < arrFilters.length; i++) {
-        var newFilter = $('<div/>').attr({id: "filter-"+arrFilters[i]});
-        newFilter.html(arrFilters[i]);
-        $("#filter-search").append(newFilter);
-        }
-    addFilterEventHandlers(arrFilters);
-    }
-
-//Attaches on-click events for the filters
-function addFilterEventHandlers(arrFilters) {
-    for (i=0; i < arrFilters.length; i++) {
-        $("#filter-"+arrFilters[i]).click(function() {
-            //Hides the selected filter
-            $(this).animate({opacity:"0"}, 400, "swing", function() {
-                $(this).css("visibility", "hidden");
-            });
-            //Has it show up underneath the Filters Applied area
-            //Adds a text box for input and a green plus button to submit
-            var filterText = $(this).text();
-            var selectedFilter = $("<div>"+filterText+"</div>");
-            selectedFilter.attr({id:"selected-" + filterText, class: "selected-Filters"});
-            var filterTextInput = $('<input/>').attr({ type: "text", name:"filter", value:"", id: "input-"+filterText});
-            var plusIcon = $('<img>').attr({src: "images/green_plus.png", class: "green-plus", id: "green-plus-"+filterText});
-            var minusIcon = $('<img>').attr({src: "images/red-minus.png", class: "red-minus", id: "red-minus-"+filterText});
-            $("#filter-container").append(selectedFilter);
-            $(selectedFilter).append("<br/>");
-            $(selectedFilter).append(filterTextInput);
-            $(selectedFilter).append(plusIcon);
-            $(selectedFilter).append(minusIcon);
-            $("#filter-container").css("visibility", "visible");
-            
-            //Adds event handler to remove green button upon submission
-            $("#green-plus-"+filterText).click(function() {
-                $(this).animate({opacity:"0"}, 800, "swing", function() {
-                    $(this).css("visibility", "hidden");
-                    $("#input-"+filterText).css("visibility", "hidden");
-                });
-                var userFilterInput = $("#input-"+filterText).val().trim();
-                $("#selected-"+filterText).text(userFilterInput);
-            });   
-        });
-    };
-};
-
-function GoToSignUp() {
-    $("#login-div").animate({opacity: "0"});
-}
-
-function LogInOrSignUpPrompt() {
-    var LoginDiv = $("<div>Login</div>").attr({id: "login-div"});
-    var SignUpDiv = $("<div>Sign-up</div>").attr({id: "signup-div"});
-    $("#search-submit-shadow").append(LoginDiv);
-    $("#search-submit-shadow").append(SignUpDiv);
-    $("#login-div").click(function (){
-        DisplayLoginInputSubmit();
-    });
-    $("#signup-div").click(function (){
-        GoToSignUp();
-        connectToSignUpPage();
-    });
-}
-
-function DisplayLoginInputSubmit() {
-    $("#signup-div").animate({opacity: "0"});
-    var loginForm = $('<form action="/login" method="get" id="signup-form"></form>');
-    var usernameInput = $('<input type="username" id="username-input" value="username" name="username"><br>;');
-    var passwordInput = $('<input type="password" id="password-input" password="password" name="password"><br>;');
-    var submitButton  = $('<input type="submit" id="login-submit" value="Submit"> ');
-    $(loginForm).append(usernameInput);
-    $(loginForm).append(passwordInput);
-    $(loginForm).append(submitButton);
-    $("#search-submit-shadow").append(loginForm);
-}
 //Attaches on-click events for the filtersS
 
-$(document).ready(function(){
-
-    $("#search-submit-shadow").click(function() {
-        $("#search-submit-shadow").off("click");
-        event.preventDefault();
-        $(this).animate({height: "70%", width: "70%", top: "10%", left: "15%", opacity:"0.7"});
-        $("#home-prompt-1").animate({opacity:"0"});
-        $("#home-prompt-2").animate({opacity:"0"});
-        $("#search-submit").animate({opacity:"0"});
-        $(this).animate({opacity:"0.7"});
-
-
-        buildWireFrame();
-        //GrabsUserData();
-
-        //LogInOrSignUpPrompt();
-
-
-        //Runs first API call
-        //startMenuCall();
-
-        //Runs first API call
-        //startMenuCall();
-
-        //Populate the overlay page
-        
-        //Prevents re-clicks
+function startMenuCall () {
+        $.get({
+            url: "/api/restaurant",
+            method: "GET"
+        }).done(function(response) {
+            console.log(response);
+            console.log(response[0].restaurant);
+            for (i=0; i < 10; i++) {
+                console.log("A");
+                var restaurantDiv = $("<div></div>");
+                $(restaurantDiv).attr("class", "row");
+                $(restaurantDiv).attr("id", "results-display");
+                var firstColumn = $("<div>dgdfg</div>");
+                $(firstColumn).attr("class", "col s1");
+                $(firstColumn).text(response[i].restaurant);
+                $(restaurantDiv).append(firstColumn);
+                $("#search-submit-shadow").append(restaurantDiv);
+            }  
         });
+    };
+
+function hideCode() {
+
+
+}
+
+$(document).ready(function(){
+        startMenuCall();
 });
